4d802f2658086f43d873bfe1d58cab22a3b9a90d
Finleyw <511346@chrome.escg.ac.uk>
2021-10-06 10:11:39 +0100

    aaaaa

--- Assets/Scripts/MovePlayers.cs
+++ Assets/Scripts/MovePlayers.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MovePlayers : MonoBehaviour 
 {
 
-	public float moveSpeed = 05f;
+	public float moveSpeed = 0.1f;
 	public JoyStick jsMovement;
 
 	
@@ -21,16 +21,25 @@ public class MovePlayers : MonoBehaviour
 		
 			transform.position += direction * moveSpeed;
 			transform.position = new Vector3(Mathf.Clamp(transform.position.x,xMin,xMax),Mathf.Clamp(transform.position.y,yMin,yMax),0f);//to restric movement of player
-		}	
+
+		}
+		/*if(jsMovement.InputDirection.y>0);
+        {
+			transform.rotation = 0;
+        }
+		if (jsMovement.InputDirection.y < 0)
+        {
+			transform.rotation = 180;
+		}*/
 	}	
 	
 	void Start()
     {
 		
 		//Initialization of boundaries
-		xMax = Screen.width - 50; // I used 50 because the size of player is 100*100
-		xMin = 0; 
-		yMax = Screen.height - 50;
-		yMin = 0;
+		xMax = 10; // I used 50 because the size of player is 100*100
+		xMin = -10; 
+		yMax = 5;
+		yMin = -5;
 	}
 }
